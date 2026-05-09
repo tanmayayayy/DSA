34e2434fb48c8f029b3aa27d1607c1e0f32b9606
tanmayayayy <tanmaychhibber10@gmail.com>
2026-05-10 00:15:57 +0530

    Time: 0 ms (100%), Space: 8.1 MB (37.25%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+
+        
+        vector<int> digits;
+
+        while(num > 0){
+            digits.push_back(num % 10);
+            num /= 10;
+        }
+
+        reverse(digits.begin(), digits.end());
+
+        for(int i = 0; i<digits.size();i++){
+            if(digits[i]==6){
+                digits[i]=9;
+                break;
+            }
+        }
+
+        int ans = 0;
+
+        for(int d : digits){
+        ans = ans * 10 + d;
+        }
+
+        return ans;
+    }
+};
